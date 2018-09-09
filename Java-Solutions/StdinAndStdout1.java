c7807690d52a39712fdf4964710826a0faec9346
joakiols <joakiols@kth.se>
2018-09-09 21:41:31 +0200

    stdin edit

---
+++ Java-Solutions/StdinAndStdout1.java
@@ -0,0 +1,19 @@
+/**
+ * @Author: Joakim Olsson <lomo133>
+ * @Date:   2018-09-09T21:40:35+02:00
+ * @Last modified by:   lomo133
+ * @Last modified time: 2018-09-09T21:40:35+02:00
+ */
+
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        System.out.println(a);
+    }
+}
--- Java-Solutions/WelcomeToJava.java
+++ Java-Solutions/WelcomeToJava.java
@@ -6,7 +6,6 @@
  */
 
 
-
 public class Solution {
 
     public static void main(String[] args) {
