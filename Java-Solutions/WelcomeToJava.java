338683f6f303d5d0c1f1bd86c00d001775ebc39f
joakiols <joakiols@kth.se>
2018-09-09 21:33:08 +0200

    welcometojave edit

--- Java-Solutions/WelcomeToJava.java
+++ Java-Solutions/WelcomeToJava.java
@@ -1,3 +1,12 @@
+/**
+ * @Author: Joakim Olsson <lomo133>
+ * @Date:   2018-09-09T21:16:18+02:00
+ * @Last modified by:   lomo133
+ * @Last modified time: 2018-09-09T21:30:29+02:00
+ */
+
+
+
 public class Solution {
 
     public static void main(String[] args) {
